9bf299c0040dc497929d1db0dc8c38d2fa44180e
groundzero099 <arafin96@gmail.com>
2023-01-22 16:21:07 +0600

    update

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const dbConnect = async() => {
+const dbConnect = async () => {
   const uri =
-   // `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.iytrdbr.mongodb.net/?retryWrites=true&w=majority`;
+    // `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.iytrdbr.mongodb.net/?retryWrites=true&w=majority`;
+    //`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.aevyxel.mongodb.net/?retryWrites=true&w=majority`;
     `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.aevyxel.mongodb.net/?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri, {
+  const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverApi: ServerApiVersion.v1,
   });
   await client.connect();
   const userCollection = client.db("attendance_db").collection("users");
-  const attendanceCollection = client.db("attendance_db").collection("attendance");
+  const attendanceCollection = client
+    .db("attendance_db")
+    .collection("attendance");
   return {
     userCollection: userCollection,
-    attendanceCollection: attendanceCollection
-  }
+    attendanceCollection: attendanceCollection,
+  };
 };
 module.exports = dbConnect;
